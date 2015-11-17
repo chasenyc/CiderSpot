793c07c1f12db86e3ac8b07dd77fb62b944df9f5
Alex Harris <chasenyc@gmail.com>
2015-11-17 18:06:17 -0500

    Adjust API json response to include style.

--- app/assets/javascripts/components/cider_detail_view.js.jsx
+++ app/assets/javascripts/components/cider_detail_view.js.jsx
@@ -1,11 +1,41 @@
 var CiderDetailView = React.createClass({
 
-  
+  getInitialState: function () {
+    var ciderId = this.props.params.ciderId;
+    var cider = this._findCiderById(ciderId) || {} ;
+    return { cider: cider };
+  },
+
+  componentDidMount: function () {
+    CiderStore.addChangeListener(this.changed);
+    ApiUtil.fetchCiders();
+  },
+
+  changed: function () {
+    var ciderId = this.props.params.ciderId;
+    var cider = this._findCiderById(ciderId) || {} ;
+    this.setState({cider: cider});
+  },
+
+  _findCiderById: function (ciderId) {
+    var result;
+    CiderStore.all().forEach(function(cider) {
+      if (parseInt(ciderId) === cider.id) {
+        result = cider;
+      }
+    });
+    return result;
+  },
 
   render: function () {
+    console.log(this.state.cider);
     return (
       <div className="cider-detail">
-        Cider Detail View
+        <img className="cider-thumb" src={this.state.cider.image_url}></img>
+        <h1 className="Name">Name: {this.state.cider.name}</h1>
+        <h2 className="Brewery">Brewery: {this.state.cider.brewery_id}</h2>
+        <h3 className="Style">Style: {this.state.cider.style}</h3>
+
       </div>
     );
   }
