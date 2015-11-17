d8576b0f46d8bdbebffd4c3457ac0b824f2acd3c
Alex Harris <chasenyc@gmail.com>
2015-11-17 13:47:08 -0500

    Add reviews partial to cider#show.

--- app/views/api/ciders/show.json.jbuilder
+++ app/views/api/ciders/show.json.jbuilder
@@ -1,1 +1,5 @@
 json.extract! @cider, :id, :name, :description, :brewery_id, :style, :organic, :abv, :image_url
+
+json.reviews do
+  json.partial! 'api/reviews/show', collection: @cider.reviews, as: :review
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@ cider_names = %w(apple banana orange maple fruit coal filthy sriracha)
 cider_names.each do |name|
   Cider.create!(name: "#{name} cider", description: "wonderful #{name}, its a great cider.", brewery_id: Random.rand(11), style: Random.rand(11), abv: Random.rand(100), organic: 'Y')
 end
+
+titles = %w(wonderful terrible lovely dirty filthy smelly neveragain! horrid bestthingtoentermymouthallday)
+
+8.times do |i|
+  Review.create!(user_id: 1, cider_id: i+1, title: titles.sample, look_rating: Random.rand(5), smell_rating: Random.rand(5), taste_rating: Random.rand(5), feel_rating: Random.rand(5), overall_rating: Random.rand(5))
+
+  Review.create!(user_id: 1, cider_id: i+1, title: titles.sample, look_rating: Random.rand(5), smell_rating: Random.rand(5), taste_rating: Random.rand(5), feel_rating: Random.rand(5), overall_rating: Random.rand(5))
+end
