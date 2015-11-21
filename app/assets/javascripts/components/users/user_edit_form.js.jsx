8b91da461c54ad2cff408c5e72e4872494a6c15a
Alex Harris <chasenyc@gmail.com>
2015-11-21 11:18:10 -0500

    Add onchange handler to UserEditForm.

--- app/assets/javascripts/components/users/user_edit_form.js.jsx
+++ app/assets/javascripts/components/users/user_edit_form.js.jsx
@@ -1,41 +1,46 @@
 var UserEditForm = React.createClass({
 
   getInitialState: function () {
-    return ({currentUser: CurrentUserStore.currentUser()});
+    return (CurrentUserStore.currentUser());
   },
 
   componentWillReceiveProps: function (newProps) {
-    this.setState({currentUser: CurrentUserStore.currentUser()});
+    this.setState(CurrentUserStore.currentUser());
   },
 
   handleSubmit: function () {
+    e.preventDefault();
+  },
 
+  handleChange: function (e) {
+    var obj = {};
+    obj[e.target.name] = e.target.value;
+    this.setState(obj);
   },
 
   render: function () {
-    console.log(this.state.currentUser.username)
     return (
       <div className="edit-profile-form">
         <h1>Edit your profile</h1>
-        <form onSubmit={ this.handleSubmit }>
+        <form onChange={this.handleChange} onSubmit={ this.handleSubmit }>
           <label>Username:
             <input type="text"
-                   name="user[username]"
-                   defaultValue={this.state.currentUser.username} />
+                   name="username"
+                   value={this.state.username} />
           </label>
           <label>Email:
-            <input type="email" name="user[email]"
-                   defaultValue={this.state.currentUser.email} />
+            <input type="email" name="email"
+                   value={this.state.email} />
           </label>
           <label>Password:
-            <input type="password" name="user[password]" />
+            <input type="password" name="password" />
           </label>
           <label>Birthdate:
-            <input type="date" name="user[birthdate]"
-                   defaultValue={this.state.currentUser.birthdate} />
+            <input type="date" name="birthdate"
+                   value={this.state.birthdate} />
           </label>
           <label>Profile photo:
-            <input type="file" name="user[image]" />
+            <input type="file" name="image" />
           </label>
           <button>Save Changes</button>
         </form>
