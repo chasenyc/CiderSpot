ce6ea7bc5a4e26f2fbc805ebb1d3137a4020e80f
Alex Harris <chasenyc@gmail.com>
2015-11-23 12:43:52 -0500

    Fix config.

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module CiderSpotApp
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_protocol => 'http',
-      :bucket => ENV["s3_bucket"]
+      :bucket => ENV["s3_bucket"],
       :s3_credentials => {
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"]
