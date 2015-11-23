41a4aaf1aecb8909216dca18e7ac5c2b3a2d7fbb
Alex Harris <chasenyc@gmail.com>
2015-11-23 12:42:55 -0500

    Fix config.

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,9 @@ module CiderSpotApp
     config.active_record.raise_in_transactional_callbacks = true
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_protocol => 'http',
+      :bucket => ENV["s3_bucket"]
       :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"]
       }
