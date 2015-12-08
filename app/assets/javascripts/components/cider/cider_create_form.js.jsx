2ee324c9a4d9775b36325cf9b13479d3e1af26ff
Alex Harris <chasenyc@gmail.com>
2015-12-07 21:09:38 -0500

    Add CreateCiderForm.

---
+++ app/assets/javascripts/components/cider/cider_create_form.js.jsx
@@ -0,0 +1,16 @@
+var CiderCreateForm = React.createClass({
+
+  mixins: [ReactRouter.History, Analytics],
+
+  render: function () {
+
+    return (
+      <div className="cider-create-form">
+        <form>
+          
+        </form>
+      </div>
+    );
+  }
+
+});
